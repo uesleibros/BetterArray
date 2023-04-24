02f66d95cd1b72b5fb84693d60779dcd807d03a9
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2023-04-24 11:02:10 -0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,10 +1,11 @@
 #include "../include/BetterArray.h"
 #include <iostream>
 
-int main(int argc, char* argv[]) {
+int main() {
   BetterArray<int> n;
   n.add(1);
   n.add(7);
 
   std::cout << "Working! " << n[1] << std::endl;
+  return 0;
 }
