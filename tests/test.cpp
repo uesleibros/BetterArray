f119ab0b6631d1862578f1bf4d9ba5cbcafcc5cc
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2023-04-24 03:33:40 -0300

    Create test.cpp

---
+++ tests/test.cpp
@@ -0,0 +1,10 @@
+#include "include/BetterArray.h"
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  BetterArray<int> n;
+  n.add(1);
+  n.add(7);
+
+  std::cout << "Working! " << n[1] << std::endl;
+}
