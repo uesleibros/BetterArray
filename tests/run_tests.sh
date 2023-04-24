653013b144d4a7dc8d09b1173abab4cf3a2edc28
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2023-04-24 19:20:57 -0300

    Update run_tests.sh

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,10 +3,10 @@
 # Run tests for BetterArray
 
 # Compile the test executable
-g++ -Wall -Wextra -std=c++11 -O2 -I../include tests/list.cpp -o list
-g++ -Wall -Wextra -std=c++11 -O2 -I../include tests/matrix.cpp -o matrix
-g++ -Wall -Wextra -std=c++11 -O2 -I../include tests/remove.cpp -o remove
-g++ -Wall -Wextra -std=c++11 -O2 -I../include tests/adds.cpp -o adds
+g++ -Wall -Wextra -std=c++11 -O2 -I../include tests/src/list.cpp -o list
+g++ -Wall -Wextra -std=c++11 -O2 -I../include tests/src/matrix.cpp -o matrix
+g++ -Wall -Wextra -std=c++11 -O2 -I../include tests/src/remove.cpp -o remove
+g++ -Wall -Wextra -std=c++11 -O2 -I../include tests/src/adds.cpp -o adds
 
 # Run the tests
 ./list
