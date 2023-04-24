68825922ab7bce49b7b025fd0e9e9607910149f3
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2023-04-24 17:18:30 -0300

    📚 Updated BetterArray header

--- include/BetterArray.h
+++ include/BetterArray.h
@@ -1,37 +1,64 @@
 //    _                     
 //   /_\  _ _ _ _ __ _ _  _   _____________________________________________
 //  / _ \| '_| '_/ _` | || |  & A better array for C++                    &
-// /_/ \_\_| |_| \__,_|\_, |  & version 1.0                               &
+// /_/ \_\_| |_| \__,_|\_, |  & version 1.2                               &
 //                     |__/   & https://github.com/uesleibros/BetterArray &
 /**************************************************************************\
 + License: MIT                                                             -
 + Open-Source                                                              -
-+ Made with ♥️ for you.                                                    -
++ Made with ♥️ for you.                                                     -
 \**************************************************************************/
 
 #ifndef BETTER_ARRAY_H
 #define BETTER_ARRAY_H
 
-#include "Iterator.hpp"
+#include <initializer_list>
 
 template <typename T>
-class BetterArray {
+class Array {
 private:
 	T* data;
 	int size_;
 public:
-	BetterArray() : data(nullptr), size_(0) {};
-	~BetterArray();
+	class Iterator {
+		private:
+		T* ptr;
+
+		public:
+			Iterator(T* ptr) : ptr(ptr) {}
+			Iterator(const Iterator& other) : ptr(other.ptr) {}
+
+			Iterator& operator++() {
+			   ++ptr;
+			   return *this;
+			}
+
+			bool operator!=(const Iterator& it) const {
+			   return ptr != it.ptr;
+			}
+
+			T& operator*() {
+			   return *ptr;
+			}
+   };
+
+	Array() : data(nullptr), size_(0) {};
+	~Array();
 
 	T& operator[](int index);
 
 	int get_size();
 	void add(const T& value);
+	void append(const Array<T>& other);
+   void append(std::initializer_list<T> ilist);
 	void remove(int index);
 
-	Iterator<T> begin();
- 	Iterator<T> end();
+	typename Array<T>::Iterator begin();
+ 	typename Array<T>::Iterator end();
 };
 
-#include "implements/BetterArray.inl"
+#include "Multidimensional.h"
+#include "implements/Array.inl"
+#include "implements/Iterator.inl"
+
 #endif
