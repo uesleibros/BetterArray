85142e7ef4bd39cb82383028d687332ee9b85692
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2023-04-24 19:06:46 -0300

    Create Memory.c

---
+++ src/utils/Memory.c
@@ -0,0 +1,36 @@
+//    _                     
+//   /_\  _ _ _ _ __ _ _  _   _____________________________________________
+//  / _ \| '_| '_/ _` | || |  & A better array for C++                    &
+// /_/ \_\_| |_| \__,_|\_, |  & version 1.2                               &
+//                     |__/   & https://github.com/uesleibros/BetterArray &
+/**************************************************************************\
++ License: MIT                                                             -
++ Open-Source                                                              -
++ Made with ♥️ for you.                                                     -
+\**************************************************************************/
+
+void safe_free(void* ptr) {
+    if (!ptr) {
+        fprintf(stderr, "Error: Null pointer.\n");
+        exit(EXIT_FAILURE);
+    }
+    free(ptr);
+}
+
+void* safe_malloc(size_t size) {
+    void* ptr = malloc(size);
+    if (!ptr) {
+        fprintf(stderr, "Erro: Não foi possível alocar %zu bytes.\n", size);
+        exit(EXIT_FAILURE);
+    }
+    return ptr;
+}
+
+void* safe_realloc(void* ptr, size_t size) {
+    void* new_ptr = realloc(ptr, size);
+    if (new_ptr == NULL && size != 0) {
+        fprintf(stderr, "Error: Failed to allocate memory\n");
+        exit(EXIT_FAILURE);
+    }
+    return new_ptr;
+}
