a828d9093d76f9c0f5e14923b92c84ce7a24263d
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2023-04-24 18:36:56 -0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -2,20 +2,15 @@
 #include "../include/BetterArray.h"
 
 int main() {
-   ArrayMulti<int> matriz;
-   Array<int> numeros;
+   Array<int> numeros = {};
 
    numeros.add(2);
    numeros.add(7);
    numeros.add(8);
    numeros.add(1);
 
-   numeros.append({1, 2, 3});
+   numeros[4] = {};
 
-   matriz[0] = {};
-   matriz[0].append({74, 87, 32, 5});
-
-   std::cout << matriz[0].get_size() << std::endl;
    for (Array<int>::Iterator it = numeros.begin(); it != numeros.end(); ++it) {
       std::cout << *it << " ";
    }
