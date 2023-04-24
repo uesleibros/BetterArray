4986ffb8066794849f777874bc22f373ae1ec3f0
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2023-04-24 03:36:22 -0300

    Update run_tests.sh

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,7 +3,7 @@
 # Run tests for BetterArray
 
 # Compile the test executable
-g++ -Wall -Wextra -std=c++11 -O2 -I../include ./tests/test.cpp -o test
+g++ -Wall -Wextra -std=c++11 -O2 -I../include tests/test.cpp -o test
 
 # Run the tests
 ./test
